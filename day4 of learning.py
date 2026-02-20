e4e14c43128c331576b7e0d71da3b1307bef9f9c
santhiya456 <santhiyanithya42@gmail.com>
2026-02-20 20:31:02 +0530

    Add files via upload

---
+++ day4 of learning.py
@@ -0,0 +1,11 @@
+score=int(input("score:"))
+if(score>90):
+    print("A")
+elif(score>70):
+    print("B")
+elif(score>50):
+    print("C")
+elif(score>49):
+    print("D")
+else:
+    print("Fail")
