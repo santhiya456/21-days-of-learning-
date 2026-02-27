1b9c38ddea317af12cdec3f5e5920944992b9038
santhiya456 <santhiyanithya42@gmail.com>
2026-02-27 19:19:05 +0530

    Add files via upload

---
+++ day11 of learning2.py
@@ -0,0 +1,6 @@
+a=[1,2,3,4,5,6,7,8,9]
+a.insert(4,23)
+a.append(45)
+a.remove(2)
+a.pop(7)
+print(a)
---
+++ day11 of learning3.py
@@ -0,0 +1,11 @@
+b={
+    "name":"santhiya",
+    "age":"19",
+    "native":"Tvl",
+    "collage":"sathyabama university",
+    "batch":"2024-2028"
+    }
+b.update({"native":"tirunelveli"})
+b["class number"]="123"
+b.pop("native")
+print(b)
