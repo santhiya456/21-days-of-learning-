1a3725a7c8ee961e47f31a51a95ea97442a64381
santhiya456 <santhiyanithya42@gmail.com>
2026-02-28 17:31:50 +0530

    Add files via upload

---
+++ day12 of learning.py
@@ -0,0 +1,28 @@
+def add():
+    print("Addition")
+    a=int(input("Enter a:"))
+    b=int(input("Enter b:"))
+    print(a+b)
+
+def sub():
+    print("Subtraction")
+    a=int(input("Enter a:"))
+    b=int(input("Enter b:"))
+    print(a-b)
+
+def mul():
+    print("Multiplication")
+    a=int(input("Enter a:"))
+    b=int(input("Enter b:"))
+    print(a*b)
+
+def div():
+    print("Division")
+    a=int(input("Enter a:"))
+    b=int(input("Enter b:"))
+    print(a/b)
+
+add()
+sub()
+mul()
+div()
---
+++ day12 of learning2.py
@@ -0,0 +1,8 @@
+def findoddoreven(a):
+    if(a%2==0):
+        print("Even")
+    else:
+        print("Odd")
+        
+b=int(input("Enter b:"))
+findoddoreven(b)
