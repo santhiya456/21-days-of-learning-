013e83bdc627929560191dda52c2a32097add495
santhiya456 <santhiyanithya42@gmail.com>
2026-03-02 20:13:02 +0530

    Add files via upload

---
+++ day14 of learning2.py
@@ -0,0 +1,18 @@
+class laptop:
+    price=0
+    proc=""
+    ram=""
+
+hp=laptop()
+dell=laptop()
+
+hp.price=5000
+hp.proc="i5"
+hp.ram="8gb"
+
+dell.price=6000
+dell.proc="i7"
+dell.ram="16gb"
+
+print(hp.price)
+print(dell.price)
